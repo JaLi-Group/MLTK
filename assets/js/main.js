828fb3647121e4145a589dffde188c92814c19e2
Jacques Plante <115102175+XLution@users.noreply.github.com>
2026-09-15 14:05:14 -0400

    Add toggleMenu function for navigation menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,11 @@
 // Placeholder for future scripts
 console.log("JaliGroup main site loaded.");
+
+function toggleMenu() {
+    const menu = document.getElementById("navMenu");
+    if (menu.style.maxHeight) {
+        menu.style.maxHeight = null;
+    } else {
+        menu.style.maxHeight = menu.scrollHeight + "px";
+    }
+}
